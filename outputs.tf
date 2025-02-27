d6de34712ff547de258b848ea9d3ee892332f3ab
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-27 12:00:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "instance_public_ip" {
+output "instance_public_ip" { #jo b ec2 instance bnega uski public ip print kr dega ye
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.nginxserver.public_ip
 }
@@ -6,4 +6,5 @@ output "instance_public_ip" {
 output "instance_url" {
   description = "The URL to access the Nginx server"
   value       = "http://${aws_instance.nginxserver.public_ip}"
+#can access it from  this url to see ngnix page
 }
