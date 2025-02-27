dbe94e9888c9b9dd2270afef40d8a87f75260495
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-02-27 11:53:37 +0530

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -2,22 +2,22 @@ resource "aws_security_group" "nginx-sg" {
   vpc_id = aws_vpc.my_vpc.id
 
   #Inbound rule for HTTP
-  ingress {
+  ingress { #incoming request example we upload a file on cloud aws
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # khai se bhi access koi se b ip se
   }
 
   #Outbound rule
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" # all protocol allowed 
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
+  tags = { #ye toh bss naam h apne sg ka
     Name = "nginx-sg"
   }
 }
